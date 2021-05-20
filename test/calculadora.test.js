2ca48c3e11697492b5e08cf08cb9dba61fcdec7f
gilvalencia <gilvalencia86@gmail.com>
2021-05-20 19:24:59 +0200

    Prueba de suma creada

---
+++ test/calculadora.test.js
@@ -0,0 +1,14 @@
+const { test } = require("@jest/globals");
+
+
+test("La suma de 2 y 5 devuelve 7", () => {
+    // arrange
+    const a = 2;
+    const b = 5;
+    const esperado = 7;
+    // act
+    const resultado = suma(a, b);
+
+    // arrange
+    expect(resultado).toBe(esperado)
+})
