994a80ab2da005de79ce0d4abd21f841a408c6ea
miguelalfr <miguel_alfr@hotmail.com>
2021-05-22 21:58:55 +0200

    Funcion raiz creada e implementada

--- modules/calculadora.raiz.js
+++ modules/calculadora.raiz.js
@@ -1,5 +1,12 @@
 const raiz = (a) => {
-    
+
+    var n = 0
+
+    while (n * n < a) {
+        n = n + 1;
+    }
+
+    return division(suma((potencia(n, 4)), (producto(6, producto(potencia(n, 2), a)))), suma(producto(4, potencia(n, 3)), producto(producto(4, n), a)));
 
 }
 
