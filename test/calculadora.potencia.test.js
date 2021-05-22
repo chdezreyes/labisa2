ead53ac60a811e107ca088e25c065050f493a3c4
CarlosHernandez <carlos@gextiona.com>
2021-05-22 20:01:51 +0100

    funcion potencia implementada

--- test/calculadora.potencia.test.js
+++ test/calculadora.potencia.test.js
@@ -4,8 +4,8 @@ const potencia = require("../modules/calculadora.potencia");
 test("La potencia de 2 elevado a 4 devuelve 16", () => {
     //arrange
     const a = 2;
-    const b = 4;
-    const esperado = 16;
+    const b = 6;
+    const esperado = 64;
     //act
     const resultado = potencia(a, b);
     //assert
